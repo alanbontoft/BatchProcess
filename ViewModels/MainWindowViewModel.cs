4afa32a58f83ee1806dbf3bafe9071c6a6d28c4a
alanbontoft <alan.bontoft@gmail.com>
2026-02-27 17:51:06 +0000

    SkiaSharp Nuget Version rolled back to 3.116.1 to eliminate problem with libraries on RPi

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,22 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExCSS;
 
 namespace BatchProcess.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     [ObservableProperty]
+    private int _count = 0;
+    
+    [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
+
+    [RelayCommand]
+    private void Home()
+    {
+        Debug.WriteLine("HOME");
+        Count++;
+    }
 }
