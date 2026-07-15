5b63c4c099250fec39ac0d38ac6e6f2680c7fca6
AlanBontoft-Druck <alan.bontoft@BakerHughes.com>
2026-07-15 16:38:45 +0100

    Main window modified for Kiosk app. Application exit added to Settings view.

--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using BatchProcess.Models;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using BatchProcess.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace BatchProcess.ViewModels;
@@ -11,5 +15,14 @@ public partial class SettingsPageViewModel : PageViewModelBase
     public SettingsPageViewModel()
     {
         PageName = ApplicationPageNames.SETTINGS;
-    } 
+    }
+
+    [RelayCommand]
+    void Shutdown()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
+    }
 }
